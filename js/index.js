2b686d858a64a373cba6bc9430ce925233198887
sdeep153 <sdeep1534@@gmail.com>
2023-11-23 16:42:36 +0530

    added restart game option

--- js/index.js
+++ js/index.js
@@ -1,47 +1,68 @@
-var bubble_val = 0;
-var hit_val = 0;
-var score_val = 0;
+var HighScore = 0;
 
-const MakeBubble = () => {
-    var clutter=""
+const StartNewGame = () => {
+    var bubble_val = 0;
+    var hit_val = 0;
+    var score_val = 0;
 
-    for(var i=0; i<110; i++){
-        bubble_val = Math.floor(Math.random()*10)
-        clutter += `<div class="bubble"><h3>${bubble_val}</h3></div>`
+    const MakeBubble = () => {
+        var clutter=""
+
+        for(var i=0; i<110; i++){
+            bubble_val = Math.floor(Math.random()*10)
+            clutter += `<div class="bubble"><h3>${bubble_val}</h3></div>`
+        }
+
+        document.querySelector('#pbtm').innerHTML = clutter
     }
 
-    document.querySelector('#pbtm').innerHTML = clutter
-}
+    const getNewHit = () => {
+        hit_val = Math.floor(Math.random()*10)
+        document.querySelector('#hit').innerHTML = hit_val
+    }
 
-const getNewHit = () => {
-    hit_val = Math.floor(Math.random()*10)
-    document.querySelector('#hit').innerHTML = hit_val
-}
-
-const RunTimer = () => {
     var timer_val = 60;
-    
-    var time_interval = setInterval(() => {
-        if(timer_val > 0){
-            timer_val--;
-            document.querySelector('#timer').textContent = timer_val;
-        }
-        else{
-            clearInterval(time_interval)
-            document.querySelector('#pbtm').innerHTML = `<h2>Game over<br>Your Score : ${score_val}</h2>`
-        }
-    }, 1000)
-    
-}
 
-const AddScore = () => {
-    score_val += 10;
-    document.querySelector('#score').textContent = score_val
-}
+    const showTimer = () => {
+        document.querySelector('#timer').textContent = timer_val;
+    }
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('pbtm').addEventListener('click', (details) => {
-        var clicked_num = Number(details.target.textContent) 
+    const RunTimer = () => {
+        
+        var time_interval = setInterval(() => {
+            if(timer_val > 0){
+                timer_val--;
+                showTimer()
+            }
+            else{
+                clearInterval(time_interval)
+                document.querySelector('#pbtm').innerHTML = `
+                                                                <div id='game_over'>
+                                                                    <h2>Game over<br>Your Score : ${score_val}</h2>
+                                                                    <br> 
+                                                                    <h3 id='restart' class='restart_btn'>Play Again</h3>
+                                                                </div>
+
+                                                            `
+                HighScore = Math.max(HighScore, score_val)
+                console.log(HighScore)
+            }
+        }, 1000)
+        
+    }
+
+    const showScore = () => {
+        document.querySelector('#score').textContent = score_val
+    }
+
+    const AddScore = () => {
+        score_val += 10;
+        showScore()
+    }
+
+    
+    document.getElementById('pbtm').addEventListener('click', (event) => {
+        var clicked_num = Number(event.target.textContent) 
         
         if(clicked_num === hit_val){
             AddScore()
@@ -49,9 +70,29 @@ document.addEventListener('DOMContentLoaded', () => {
             getNewHit()
         }
     })
-})
 
+    document.getElementById('pbtm').addEventListener('click', function(event) {
+        if (event.target && event.target.id === 'restart') {
+            timer_val=60;
+            score_val=0;
+            MakeBubble()
+            getNewHit()
+            showTimer()
+            RunTimer()
+            showScore()
+            showHighScore()
+        }
+    });
 
-MakeBubble()
-getNewHit()
-RunTimer()
+    const showHighScore = () => {
+        document.querySelector('#high_score').textContent = HighScore
+    }
+
+    MakeBubble()
+    getNewHit()
+    showTimer()
+    RunTimer()
+    showHighScore()
+}
+
+StartNewGame()
